99927d84ae0f692511ba9e733ea589ac12555c15
Vikas5999 <vikasdon95@gmail.com>
2026-05-01 17:46:12 +0530

    New file updated

---
+++ casting.py
@@ -0,0 +1,18 @@
+#Type Casting
+#var1=float(1000)
+#print(type(var1))
+#print(var1)
+
+#num1="50"
+#num2="20"
+#res=int(num1)+int(num2)
+#print(res)
+#print(type(res))
+
+
+num1="50"
+num2="20"
+print(type(num1),type(num2))
+res=str(num1)+str(num2)
+print(res)
+print(type(res))
--- first.py
+++ first.py
@@ -1,2 +1,8 @@
-print ("hello world")
+#n=10.0
+#m=10.0
+#print(n+m)
+num="100"
+name="Vikas"
+#print(num+" "+name)
+
 
