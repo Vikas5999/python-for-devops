085c11122d2441f99691363346879b0846daba10
Vikas5999 <vikasdon95@gmail.com>
2026-05-05 10:57:57 +0530

    update file

--- 4-may.py
+++ 4-may.py
@@ -39,7 +39,42 @@
 
 # #Logical opr(and,or,not)
 
-# n1=1
-# n2=3
-# res=n1==1 and n2==3 and n1==2 and n2==3
+n1=1
+n2=3
+#res=n1==1 and n2==3 and n1==2 and n2==3
+#res=not(n1==1 or n2==3 or n1==2 or n2==3)
+#print(res)
+
+
+# 5.Membership opr
+
+#str1="this is python for devops"
+#find="this"
+#print(find in str1)
+
+#num1="4567"
+#print("4" not in num1)
+
+#email="iqindia123@yahoo.com"
+#find="@gmail.com"
+#print(find in email)
+
+#email="iqindia123@yahoo.com"
+#find="@yahoo.com"
+#if find in email:
+ #   print("This is valid google mail id")
+#else:
+    #print("Invalid Email")
+
+#aman_age=20
+#abhishek_age=30
+#if aman_age > abhishek_age:
+ #   print("Yes aman's age is more than abhishek")
+#else:
+ #   print("NO aman's age is not more than abhishek")
+
+  # user is eligble for voting.
+
+min_age=18
+nationality=in
 
