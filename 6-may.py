37cf771bfeb1b5260d252603bd1e3f179eb6488a
Vikas5999 <vikasdon95@gmail.com>
2026-05-07 14:15:05 +0530

    update

---
+++ 6-may.py
@@ -0,0 +1,56 @@
+# Identify opr (in , not in)
+
+#a=10
+#b=10
+#print(id(a))
+#print(id(b))
+
+#conditional statement (0 is false)
+
+
+
+
+
+
+
+
+# wap to check number given by user in odd or even
+
+
+
+
+
+
+
+
+
+# wap to print the last digit of a number is 456735(% always modulus to find last digit and divided by 10)
+
+#print(456735%10)
+
+# wap to find largest
+# wap to find smallest
+
+#num1=3
+#num2=9
+#num3=12
+
+#if num1>=num2 and num1>=num3 :
+ #   print(f"{num1}is greatest")
+
+#if num2>=num1 and num2>=num3 :   
+#    print(f"{num2}is greatest") 
+
+#if num3>=num1 and num3>=num2 :   
+#    print(f"{num3}is greatest")  
+
+
+
+
+
+
+
+ 
+
+
+
---
+++ 7-may.py
@@ -0,0 +1,22 @@
+# name="Dev"
+# if name=="Dev":
+#     print("Yes name is provided")
+# else:
+#     print("Name is not provided")
+
+# name="aman"
+# if name=="Dev":
+#     print("Yes name dev is provided")
+# else:
+#     print("Name is not provided")
+
+# name="aman"
+# if name:
+#     print(f"Yes name {name} is provided")
+# else:
+#     print("Name is not provided")
+# 
+
+
+
+
